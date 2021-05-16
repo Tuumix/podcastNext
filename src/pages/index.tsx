8c2782c6ca2678847583530a7e4e09367583a5b4
Tuumix <wellz-tkn@hotmail.com>
2021-05-16 15:07:28 -0300

    getStaticProps implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import Header from '../components/Header'
 
-const Home: React.FC = () => {
+const Home: React.FC = (props) => {
+  console.log(props);
   return (
-    <div></div>
+    <div>
+      <p>{JSON.stringify(props)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, 
+  }
 }
 
 export default Home;
